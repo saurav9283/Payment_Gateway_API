5f92e975d8ee844d0d939771b547a688888e412d
Saurav <sauravkumar00108@gmail.com>
2024-05-01 02:19:10 +0530

    Update app.js to export app and include dotenv configuration

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express, { urlencoded } from "express"
-export const app = express();
 import cors from "cors"
 import dotenv from "dotenv";
-
-dotenv.config(); 
 import paymentRouter from "./routes/paymentRouter.js"
+export const app = express();
+
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+dotenv.config(); 
 
 
 
